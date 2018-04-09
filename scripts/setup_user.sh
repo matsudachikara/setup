096bbd44e8d9261c08f0a14af0dd3c4848dea4d9
松田 力 <matsuda.chikara@COSPAPC220.local>
2018-04-09 12:30:00 +0900

    色々追加

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -20,3 +20,16 @@ send -- \"exit\n\"
 echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
 echo "${USERNAME} ALL=(ALL:ALL)   ALL" | sudo EDITOR='tee -a' visudo
 echo "${USERNAME} ALL=(ALL:ALL)       NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
+
+expect -c "
+spawn su - ${USERNAME}
+expect \"Password:\"
+send -- \"${PASSWORD}\n\"
+"
+
+mkdir ~/.ssh
+mv id_rsa.pub ~/.ssh/
+cd ~/.ssh/
+cat id_rsa.pub >> authorized_keys
+rm id_rsa.pub
+chmod 600 authorized_keys
