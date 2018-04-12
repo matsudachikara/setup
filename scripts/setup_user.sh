276975ed34ef06cfe2f2aa33e75c2b905ba5db8d
matsuda.chikara <matsudachikara@yahoo.co.jp>
2018-04-12 12:00:28 +0900

    設定用変数初期化

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -18,7 +18,7 @@ send -- \"exit\n\"
 "
 
 echo "${USERNAME} ALL=(ALL:ALL)   ALL" | sudo EDITOR='tee -a' visudo
-echo "${USERNAME} ALL=(ALL:ALL)       NOPASSWD: ALL" | sudo EDITOR='tee -a'
+echo "${USERNAME} ALL=(ALL:ALL)       NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 
 expect -c "
 spawn su - ${USERNAME}
@@ -27,16 +27,30 @@ send -- \"${PASSWORD}\n\"
 "
 
 # vagrantで移動した公開鍵をauthorized_keysに追加
-mkdir ~/.ssh
+if [ ! -e ~/.ssh ]; then
+  mkdir ~/.ssh
+fi
 sudo chmod 700 ~/.ssh
 cd ~/.ssh/
-sudo cat /home/vagrant/.ssh/id_rsa.pub >> authorized_keys
-sudo rm /home/vagrant/.ssh/id_rsa.pub
+
+if [ ! -e ~/.ssh/authorized_keys ]; then
+  touch ~/.ssh/authorized_keys
+fi
 sudo chmod 600 authorized_keys
 
-if [! -e ~/.ssh/config ]
-  then
+if [ -e /home/vagrant/.ssh/id_rsa.pub ]; then
+  sudo cat /home/vagrant/.ssh/id_rsa.pub >> authorized_keys
+  sudo rm /home/vagrant/.ssh/id_rsa.pub
+fi
+
+if [ ! -e ~/.ssh/config ]; then
+  touch ~/.ssh/config
+fi
+if [ -e ~/.ssh/config ]; then
   echo 'ServerAliveInterval 30' >> ~/.ssh/config
   echo 'ServerAliveCountMax 3' >> ~/.ssh/config
 fi
 sudo chmod 600 ~/.ssh/config
+
+# vagrantユーザーを削除、ホームフォルダは一応残す
+sudo userdel vagrant
